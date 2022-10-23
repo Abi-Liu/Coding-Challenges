3ff1b187045a598a615e5175391a282791b6ec19
Abi Liu <abiliu018@gmail.com>
2022-10-23 14:00:00 +0100

    Coding challenge October 23, 2022

---
+++ 10-23-2022/main.js
@@ -0,0 +1,5 @@
+// Your job is to add a method 'sum' to all array objects, which sums all the numbers in it. You should return 0 if the array is empty.
+
+Array.prototype.sum = function () {
+  return this.reduce((acc, x) => acc + x, 0);
+};
