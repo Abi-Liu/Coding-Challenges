6a7b193df0390c0576ccbd434a2d386134dfd985
Abi Liu <abiliu018@gmail.com>
2022-11-08 10:37:01 -0800

    Coding Challenge for November 8, 2022

--- 11-8-2022/main.js
+++ 11-8-2022/main.js
@@ -4,7 +4,7 @@
 function getMiddle(s)
 {
   if(s.length %2 === 1){
-    return s[Math.floor(s.length/2)] 
+    return s[Math.floor(s.length/2)]  
   } else {
     return s.slice(Math.floor(s.length/2-1), Math.floor(s.length/2 + 1))
   }
